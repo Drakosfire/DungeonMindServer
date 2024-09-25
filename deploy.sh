65b65032e6aee182aed4ea8499aeacc3c6e1c2a0
Alan Meigs <drakosfire@gmail.com>
2024-09-24 22:48:56 -0600

    mounting saved_data folder

--- deploy.sh
+++ deploy.sh
@@ -36,6 +36,6 @@ docker build -t dungeonmind-image .
 
 # Run the Docker container with the specified environment file and port mapping
 echo "Running Docker container 'dungeonmind-container'..."
-sudo docker run --env-file .env -d -p 7860:7860 --name dungeonmind-container dungeonmind-image
+sudo docker run -v /var/www/DungeonMind/saved_data:/home/user/app/saved_data--env-file .env -d -p 7860:7860 --name dungeonmind-container dungeonmind-image
 
 echo "Script execution completed."
