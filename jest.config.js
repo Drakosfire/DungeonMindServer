09a9f86035ffd6820448f44b445f34b6f528f428
Alan Meigs <drakosfire@gmail.com>
2024-09-29 20:42:48 -0600

    Closer on pathing, found a blatant error in how css was pathed. Hopefully fixes

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'json'],
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
-    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-    moduleDirectories: ['node_modules', 'src'],
-};
