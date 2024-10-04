554c016173f61cb86a8a6e61d36670d75fa93e50
Alan Meigs <drakosfire@gmail.com>
2024-10-03 20:58:26 -0600

    hopefully sorted out submodule commit branch sillyness and set to main dungeonmind branch, updated deploy.sh to handle submodule and fail gracefully

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status.
+
 # Stop the Docker container
 echo "Stopping Docker container 'dungeonmind-container'..."
-docker stop dungeonmind-container
+docker stop dungeonmind-container || true  # Don't fail if container doesn't exist
 
 # Remove the Docker container
 echo "Removing Docker container 'dungeonmind-container'..."
-docker rm dungeonmind-container
+docker rm dungeonmind-container || true  # Don't fail if container doesn't exist
 
 # Remove the Docker image
 echo "Removing Docker image 'dungeonmind-image'..."
-docker rmi dungeonmind-image
+docker rmi dungeonmind-image || true  # Don't fail if image doesn't exist
 
 # Navigate to /var/www/DungeonMind directory
 echo "Navigating to /var/www/DungeonMind directory..."
 cd /var/www/DungeonMind
 
-# Clone the DungeonMind repository
-echo "Pulling the DungeonMind repository..."
-git pull
+# Update the main repository
+echo "Updating the DungeonMind repository..."
+git fetch origin
+git reset --hard origin/main  # Replace 'main' with your branch name if different
 
-# Clone the storegenerator submodule
+# Update the submodule
+echo "Updating the storegenerator submodule..."
 git submodule update --init --recursive
+git submodule update --remote --merge
 
 # Extract images using the extractimages.sh script
 echo "Extracting images..."
